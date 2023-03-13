d2055cfbf9dd7ffb854910cbec58e2527045fbe0
Dell Ward <rydelljw@gmail.com>
2023-03-13 17:46:08 -0400

    update database.js to congif for managed database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,28 +1,24 @@
  // strapi-api/config/database.js
+
+ const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
+
  module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      schema: env('DATABASE_SCHEMA'), // Not required
+      host,
+      port,
+      database,
+      user,
+      password,
       ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+        rejectUnauthorized: false,
       },
     },
     debug: false,
-    acquireConnectionTimeout: 5000,
-    pool: {
-      min: 0,
-      max: 10,
-      createTimeoutMillis: 8000,
-      acquireTimeoutMillis: 8000,
-      idleTimeoutMillis: 8000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100
-    }
   },
 });
